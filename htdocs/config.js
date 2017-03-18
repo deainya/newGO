bf13052d365a3f1df60106473414c9d5a182e4ed
deainya <deainya@gmail.com>
2017-03-18 17:19:11 +0400

    mail?

--- htdocs/config.js
+++ htdocs/config.js
@@ -6,9 +6,17 @@ module.exports = {
   port: process.env.port || 3005,
   'secret': 'secretforrfbgoinitiative',
 
-  'smpts': 'smtps://deainru%40gmail.com:mail4deainru@smtp.gmail.com',
-  'mail_addresser': '"rfbGO" <rfbGO@deain.ru>',
-  'mail_subject': 'Оповещение rfbGO ✔',
+  //'smpts': 'smtps://deainru%40gmail.com:mail4deainru@smtp.gmail.com',
+  'smtps': {
+    service: 'Gmail',
+    auth: { user: 'deainru', pass: 'mail4deainru' },
+    logger: {}, // hz
+    debug: false // include SMTP traffic in the logs
+  },
+  'defaultfields': {
+    from: '"rfbGO" <rfbGO@deain.ru>',
+    subject: 'Оповещение rfbGO ✔',
+  },
 
   database: //'mongodb://user:pass@localhost:27017/rfbgo-dev',
             'mongodb://user:pass@localhost:27017/partnergo-dev',
--- htdocs/mail.js
+++ htdocs/mail.js
@@ -4,29 +4,20 @@ let config      = require('./config'); // get our config file
 let nodemailer  = require('nodemailer'); // send emails
 
 // create reusable transporter object using the default SMTP transport
-let transporter = nodemailer.createTransport(config.smtps);
+let transporter = nodemailer.createTransport(config.smtps, config.defaultfields);
 
 module.exports = {
   sendMail(mail_to, message, htmlmessage){
     if (!htmlmessage) {
-      var mailOptions = {
-        from: config.mail_addresser, // sender address
-        to: mail_to, // list of receivers
-        subject: config.mail_subject, // subject line
-        text: message, // plaintext body
-        html: message // html body
-      };
+      var mailOptions = { to: mail_to, text: message, html: message };
     } else {
-      var mailOptions = {
-        from: config.mail_addresser, // sender address
-        to: mail_to, // list of receivers
-        subject: config.mail_subject, // subject line
-        text: message, // plaintext body
-        html: htmlmessage // html body
-      };
+      var mailOptions = { to: mail_to, text: message, html: htmlmessage };
     }
-    return transporter.sendMail(mailOptions, function(err, info){
-      if(err){ return console.log(err); } else { return console.log("The letter has been sent."); }
+    return transporter.sendMail(mailOptions, (err, info) => {
+      if (err) { return console.log(err.message); } else {
+        transporter.close();
+        return console.log("The letter has been sent. %s", info.response);
+      }
     });
   }
 }

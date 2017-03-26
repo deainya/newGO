d5ec5fcaa8d44ff10d9f1e27ca97bd1d560d3666
deainya <deainya@gmail.com>
2017-03-27 00:28:38 +0400

    what?

--- htdocs/config.js
+++ htdocs/config.js
@@ -19,7 +19,7 @@ module.exports = {
   initializeExpress: function(app, express, bodyParser) {
     app.use(bodyParser.json()); // get our request parameters
     app.use(bodyParser.urlencoded({ extended: false }));
-    app.use(express.methodOverride());
+    //app.use(express.methodOverride());
     //app.use(express.cookieParser()); // read cookies (needed for auth)
     //app.use(morgan('dev')); // use morgan to log requests to the console
     app.use( express.static(__dirname + "/../client") ); // default route

eaff05a53e8269a8657ad99b81c206bc78f1fdff
deainya <deainya@gmail.com>
2017-03-26 09:42:35 +0400

    test

--- client/scripts/services/Session.js
+++ client/scripts/services/Session.js
@@ -5,6 +5,8 @@ module.exports = function ($log, $rootScope, localStorage) {
   var _accessToken = localStorage.getItem('session.accessToken');
   console.log(_user);
   console.log(_accessToken);
+  $rootScope.user = _user;
+  $rootScope.user = _accessToken;
 
   return {
     getUser: function(){

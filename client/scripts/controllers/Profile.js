d9baf1ad1ab4b408ac9a993e3d295a86c7a49ebb
deainya <deainya@gmail.com>
2017-04-02 13:32:22 +0400

    fix?

--- client/scripts/controllers/Profile.js
+++ client/scripts/controllers/Profile.js
@@ -26,8 +26,8 @@ module.exports = function ($rootScope, $scope, $state, auth, dataSource, Gravata
   $scope.getTradepoints = function(c, r){
     $scope.points = [];
     $scope._tradepoints = true;
-    console.log({city: $rootScope.cities[c], role: r});
-    dataSource.get('/tradepoints', {city: $rootScope.cities[c], role: r}).then(function(res) {
+    console.log({city: $rootScope.cities[c].name, role: r});
+    dataSource.get('/tradepoints', {city: $rootScope.cities[c].name, role: r}).then(function(res) {
       $scope.points = res.data;
       console.log("ok");
     });

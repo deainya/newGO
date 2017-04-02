592d6ddfbbd99d7d961087e3941a4161d33ebc19
deainya <deainya@gmail.com>
2017-04-02 22:38:45 +0400

    yet not fixed

--- htdocs/routes/users.js
+++ htdocs/routes/users.js
@@ -5,7 +5,10 @@ module.exports = function(apiRoutes, jsonParser, mongo, mail) {
   apiRoutes.post("/user/delete", (req, res) => {
     let dataset = req.body.dataset || {};
     let email = dataset.email;
-    mongo.delete('users', {"email": email}, {}, (error, result) => {
+
+    console.log(req.body);
+
+    mongo.users().deleteOne({"email": email}, {}, (error, result) => {
       if (error) { res.sendStatus(400); }
       else { res.status(201).send({ success: true, message: 'User deleted' }); }
     });

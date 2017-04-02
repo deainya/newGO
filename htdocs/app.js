28b190957155c9663f9c51be578b2029508089b4
deainya <deainya@gmail.com>
2017-04-02 13:16:57 +0400

    update

--- htdocs/app.js
+++ htdocs/app.js
@@ -47,15 +47,17 @@ app.get("/tradepoints", (req, res) => {
     case 0:
       mongo.tradepoints().aggregate([{$match : {"city":city}}, {$group : { _id : "$wp", wp:{$first:"$wp"}, tradepoint:{$first:"$tradepoint"}, address:{$first:"$address"}, city:{$first:"$city"}}}]).toArray((error, docs) => {
         if(err) { res.sendStatus(400); } else { res.json( docs ); }
+        console.log("ok0");
       });
     case 1:
-      mongo.find('tradepoints', {"city":city}, {"_id":false}).toArray((error, docs) => {
+      //mongo.find('tradepoints', {"city":city}, {"_id":false}).toArray((error, docs) => {
+      mongo.tradepoints().find({"city":city}, {"_id":false}).toArray((error, docs) => {
         if(err) { res.sendStatus(400); } else { res.json( docs ); }
+        console.log("ok1");
       });
     default: ;
   }
 });
-
 
 // Socket.io Communication
 io.sockets.on('connection', socket);
